9f2aeb2302df2e83a4b481385da3b262868305ee
azkifr <azkifr@gmail.com>
2022-06-22 19:37:38 +0700

    UI buyer 6, buyer 10

--- app/src/main/java/id/hikmah/binar/secondhand/MainActivity.kt
+++ app/src/main/java/id/hikmah/binar/secondhand/MainActivity.kt
@@ -2,13 +2,14 @@ package id.hikmah.binar.secondhand
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.hikmah.binar.secondhand.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-
-    //ini notes
-    //ini juga notes
 }
---
+++ app/src/main/java/id/hikmah/binar/secondhand/ViewPagerAdapter.kt
@@ -0,0 +1,36 @@
+package id.hikmah.binar.secondhand
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class ViewPagerAdapter(private var images: List<Int>) : RecyclerView.Adapter<ViewPagerAdapter.Pager2ViewHolder>() {
+
+    inner class Pager2ViewHolder (itemView: View): RecyclerView.ViewHolder(itemView){
+        val itemImage: ImageView = itemView.findViewById(R.id.imageView2)
+
+        init {
+            itemImage.setOnClickListener{ v: View ->
+                val position = adapterPosition
+                Toast.makeText(itemView.context, "Item Clicked #${position + 1}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerAdapter.Pager2ViewHolder {
+        return Pager2ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_page, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewPagerAdapter.Pager2ViewHolder, position: Int) {
+        holder.itemImage.setImageResource(images[position])
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+}
