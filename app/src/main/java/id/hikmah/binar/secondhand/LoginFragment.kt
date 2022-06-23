6d8a24dbda76d564de1f5177f69b1507322915e6
Novian Rachman Iskandar <novian19@upi.edu>
2022-06-23 20:53:32 +0700

    add 2 function to navigate with nav component

--- app/src/main/java/id/hikmah/binar/secondhand/LoginFragment.kt
+++ app/src/main/java/id/hikmah/binar/secondhand/LoginFragment.kt
@@ -5,18 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import id.hikmah.binar.secondhand.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
-
+    private var _binding : FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnLoginClicked()
+        tvRegisterClicked()
+    }
+
+    private fun btnLoginClicked() {
+        binding.btnLogin.setOnClickListener {
+            //add action
+        }
+    }
+
+    private fun tvRegisterClicked() {
+        binding.tvDaftarDisini.setOnClickListener {
+            //add action
+        }
+    }
 
 }
