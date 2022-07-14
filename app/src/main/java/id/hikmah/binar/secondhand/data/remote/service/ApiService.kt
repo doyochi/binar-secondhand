1a8c3be4ad92573972189c179e8e53412e2932c0
Novian Rachman Iskandar <novian19@upi.edu>
2022-07-14 16:55:59 +0700

    add logic for register and validate

--- app/src/main/java/id/hikmah/binar/secondhand/data/remote/service/ApiService.kt
+++ app/src/main/java/id/hikmah/binar/secondhand/data/remote/service/ApiService.kt
@@ -4,6 +4,7 @@ import id.hikmah.binar.secondhand.data.remote.model.Product
 import id.hikmah.binar.secondhand.data.remote.model.User
 import id.hikmah.binar.secondhand.data.remote.model.notification.FavoriteProductDto
 import id.hikmah.binar.secondhand.data.remote.model.product.ProductSellerDto
+import okhttp3.RequestBody
 import retrofit2.http.*
 
 interface ApiService {
@@ -20,8 +21,15 @@ interface ApiService {
 
     //Get Favorite Product
     @GET("notification")
-    suspend fun fetchFavoriteProduct(@HeaderMap header: Map<String, String>): FavoriteProductDto
+    suspend fun fetchFavoriteProduct(@HeaderMap header: Map<String, String>): List<FavoriteProductDto>
 
+    //Get Product Seller
     @GET("seller/product")
     suspend fun fetchSellerProduct(@HeaderMap header: Map<String, String>): ProductSellerDto
+
+    //Register
+    @POST("auth/register")
+    suspend fun registerAccount(@Body body: RequestBody)
+
+    //Login
 }
--- app/src/main/java/id/hikmah/binar/secondhand/di/AppModule.kt
+++ app/src/main/java/id/hikmah/binar/secondhand/di/AppModule.kt
@@ -1,11 +1,15 @@
 package id.hikmah.binar.secondhand.di
 
+import android.app.Application
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import id.hikmah.binar.secondhand.data.local.TestDatabase
 import id.hikmah.binar.secondhand.data.remote.service.ApiClient
 import id.hikmah.binar.secondhand.data.remote.service.ApiService
+import id.hikmah.binar.secondhand.data.repository.RegisterRepository
 import id.hikmah.binar.secondhand.data.repository.SaleListRepository
 import javax.inject.Singleton
 
@@ -21,5 +25,21 @@ object AppModule {
     @Singleton
     fun provideRepositoryProductSeller(api: ApiService) : SaleListRepository {
         return SaleListRepository(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepositoryRegister(api: ApiService) : RegisterRepository {
+        return RegisterRepository(api)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTestDatabase(app: Application) : TestDatabase {
+        return Room.databaseBuilder(
+            app,
+            TestDatabase::class.java,
+            "test.db"
+        ).build()
     }
 }
