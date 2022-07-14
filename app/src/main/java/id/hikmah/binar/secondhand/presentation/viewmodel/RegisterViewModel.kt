ea524655c4a3424b34081011aa776b21af8375d6
Novian Rachman Iskandar <novian19@upi.edu>
2022-07-14 17:07:06 +0700

    add logic for register and validate

---
+++ app/src/main/java/id/hikmah/binar/secondhand/presentation/viewmodel/RegisterViewModel.kt
@@ -0,0 +1,38 @@
+package id.hikmah.binar.secondhand.presentation.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import id.hikmah.binar.secondhand.data.repository.RegisterRepository
+import id.hikmah.binar.secondhand.helper.Resource
+import kotlinx.coroutines.Dispatchers
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor(
+    private val repository: RegisterRepository
+) : ViewModel() {
+
+    var dataError = MutableLiveData<String>()
+        private set
+
+    fun registerAccount(
+        fullName: String,
+        email: String,
+        password: String
+    ) = liveData(Dispatchers.IO) {
+        emit(Resource.loading(null))
+
+        try {
+            emit(Resource.success(data = repository.registerAccount(fullName, email, password)))
+        } catch (e: IOException) {
+            emit(Resource.error(null, e.message ?: "Error Occurred!"))
+        } catch (e: HttpException) {
+            emit(Resource.error(null, e.message() ?: "Error Occurred!"))
+        }
+    }
+
+}
