b038759d5117b4b65a8453419e08c630e944a113
azkif <azkifr@gmail.com>
2022-07-24 16:25:04 +0700

    Update

--- app/src/main/java/id/hikmah/binar/secondhand/presentation/fragment/Buyer6.kt
+++ app/src/main/java/id/hikmah/binar/secondhand/presentation/fragment/Buyer6.kt
@@ -1,25 +1,46 @@
 package id.hikmah.binar.secondhand.presentation.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.textfield.TextInputLayout
 import id.hikmah.binar.secondhand.R
+import id.hikmah.binar.secondhand.data.common.hideLoading
+import id.hikmah.binar.secondhand.data.common.showLoading
+import id.hikmah.binar.secondhand.data.common.showSnackbar
 import id.hikmah.binar.secondhand.data.common.toRp
 import id.hikmah.binar.secondhand.data.local.DatabaseSecondHand
+import id.hikmah.binar.secondhand.data.remote.model.dto.buyer.PostBuyerOrderBody
 import id.hikmah.binar.secondhand.data.remote.service.ApiClient
 import id.hikmah.binar.secondhand.data.repository.BuyerRepo
+import id.hikmah.binar.secondhand.databinding.BottomSheetBinding
 import id.hikmah.binar.secondhand.databinding.FragmentBuyer6Binding
+import id.hikmah.binar.secondhand.helper.Status
 import id.hikmah.binar.secondhand.helper.mapper.BuyerProductDetailMapper
+import id.hikmah.binar.secondhand.presentation.activity.MainActivity
 import id.hikmah.binar.secondhand.presentation.adapter.ViewPagerAdapter
+import id.hikmah.binar.secondhand.presentation.viewmodel.DatastoreViewModel
 import kotlinx.android.synthetic.main.fragment_buyer6.*
 import me.relex.circleindicator.CircleIndicator3
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class Buyer6 : Fragment() {
     private var _binding: FragmentBuyer6Binding? = null
@@ -28,6 +49,8 @@ class Buyer6 : Fragment() {
     private var imagesList = mutableListOf<Int>()
     lateinit var textView: TextView
     private var productId : Int? = null
+    private lateinit var dataHarga : PostBuyerOrderBody
+    private val dataStore : DatastoreViewModel by viewModel()
 
     private val buyerRepo : BuyerRepo by lazy { BuyerRepo(
         ApiClient.instanceBuyer,
@@ -70,18 +93,27 @@ class Buyer6 : Fragment() {
         val circleIndicator = binding.circleIndicator.findViewById<CircleIndicator3>(R.id.circle_indicator)
         circleIndicator.setViewPager(view_pager2)
 
-        val bottomSheetFragment = BottomSheetFragment{
-        }
         binding.btnTertarik.setOnClickListener {
-            bottomSheetFragment.show(parentFragmentManager, "BottomSheetDialog")
+            bottomSheet()
         }
     }
+
+//    private fun showAlertDialogWithAction() {
+//        val dialog = AlertDialog.Builder(requireContext())
+//        dialog.setMessage("Anda harus login terlebih dahulu")
+//        dialog.setPositiveButton("Login") { _, _ ->
+//            startActivity(Intent(requireActivity(), MainActivity::class.java))
+//        }
+//        dialog.setNegativeButton("Batal") { _, _ ->
+//
+//        }
+//        dialog.setCancelable(false)
+//        dialog.show()
+//    }
 
     private fun getData(id: Int){
         viewModel.getProductDetail(id).observe(viewLifecycleOwner){
             it.data.let { data ->
-                Glide.with(requireContext()).load(data?.imageUrl).into(binding.ivProduk)
-                Glide.with(requireContext()).load(data?.imageUser).into(binding.ivSeller)
                 Glide.with(requireContext()).load(data?.imageUrl).into(binding.ivProduk)
                 Glide.with(requireContext()).load(data?.imageUser).into(binding.ivSeller)
                 binding.tvKota.text = data?.lokasi
@@ -90,6 +122,38 @@ class Buyer6 : Fragment() {
                 binding.deskripsi.text = data?.deskripsiBarang
                 binding.tvKategori.text = data?.kategori
                 binding.tvNamaPenjual.text = data?.username
+            }
+            val isTrue: Boolean = it.data?.id == null
+            binding.pbLoading.isVisible =  isTrue
+        }
+    }
+
+    private fun bottomSheet(){
+        val view = layoutInflater.inflate(R.layout.bottom_sheet, null)
+        val dialog = BottomSheetDialog(requireContext(),R.style.BottomSheetDialogTheme)
+        dialog.setContentView(view)
+        dialog.show()
+
+        val ivBarang = view.findViewById<ShapeableImageView>(R.id.iv_item_bid)
+        val namaBarang = view.findViewById<TextView>(R.id.tv_name_item_bid)
+        val hargaBarang = view.findViewById<TextView>(R.id.tv_price_item_bid)
+        val etHarga = view.findViewById<TextInputLayout>(R.id.et_harga)
+        val btnKirim = view.findViewById<MaterialButton>(R.id.btn_kirim)
+
+        viewModel.getProductDetail(productId!!).observe(viewLifecycleOwner){
+            it.data?.let { data ->
+                Glide.with(requireContext()).load(data.imageUrl)
+                    .into(ivBarang)
+                namaBarang.text = data.namaBarang
+                hargaBarang.text = data.hargaBarang.toRp()
+            }
+        }
+
+        btnKirim.setOnClickListener{
+            val price = etHarga.editText?.text.toString()
+            if (price == ""){
+                Toast.makeText(requireContext(),"Masukkan Harga Tawar Anda", Toast.LENGTH_SHORT).show()
+            }else{
             }
         }
     }
